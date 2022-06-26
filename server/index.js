e8c94dda12fdca0fd66972bcade677b3f2bbc791
Alinda Alvin <36437251+manlikeAlinda@users.noreply.github.com>
2022-06-26 11:35:38 -0700

    finishing up with the backend

--- server/index.js
+++ server/index.js
@@ -1,11 +1,16 @@
 const express = require('express');
 require('dotenv').config();
+const colors = require('colors');
 const {graphqlHTTP} =require('express-graphql');
 const schema = require('./schema/schema.js');
 const port = process.env.PORT || 8000;
 const bodyParser = require('body-parser');
+const connectDB = require('./config/db');
 
 const app = express();
+//connect to datatbase
+connectDB();
+
 app.use(bodyParser.json()); // application/json
 app.use('/graphql', graphqlHTTP({
     schema,
